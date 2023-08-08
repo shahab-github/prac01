6b8959c37a9b6ff8eaee3d0e0a67193500504cda
shahab-github <113800863+shahab-github@users.noreply.github.com>
2023-08-08 17:28:25 -0500

    added backend.tf

--- azure/backend.tf
+++ azure/backend.tf
@@ -1,1 +1,9 @@
 # configure the backend in the azure
+terraform {
+    backend "azurerm" {
+        resource_group_name = "tfstate"  # make sure you have this resource group
+        storage_account_name = "tfstate7786" # make sure you have this storage account already
+        container_name = "tfstate-dev"
+        key = "terraform.tfstate"
+    }
+}
