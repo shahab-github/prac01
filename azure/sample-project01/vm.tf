4fa6a0f1200dfabd6b0d570cd9032e8b7e751b98
shahab-github <113800863+shahab-github@users.noreply.github.com>
2023-08-08 12:14:57 -0500

    Create vm.tf

---
+++ azure/sample-project01/vm.tf
@@ -0,0 +1,33 @@
+# Create a virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "my-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "my-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "my-vm"
+    admin_username = "azureuser"
+    admin_password = "P@ssw0rd123"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
